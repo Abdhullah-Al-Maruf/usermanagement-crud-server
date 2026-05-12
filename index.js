836b7b47e58e08d6214eb045ec265adaf48893e8
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-05-12 11:13:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+// step 1: Load environment variables first
+require('dotenv').config();
+
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express()
+const port = 5000
+
+
+// step 2
+app.use(cors());
+app.use(express.json())
+
+//  step 3
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_SECRET}@cluster0.gklgo91.mongodb.net/?appName=Cluster0`;
+
+// step 4
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+// step 5
+const run =async()=>{
+try {
+  await client.connect();
+
+// step 6 from mongodb documentation  to get data from db
+    const db=client.db("simpleCrud");
+    const userCollection =db.collection("users")
+
+    //  make a api for the users
+app.get("/users" ,async(req,res)=>{
+
+  const cursor=userCollection.find({})
+  const result =await cursor.toArray();
+  res.send(result)
+})
+
+// --------------------------------
+
+
+     await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment app. You successfully connected to MongoDB!");
+} finally {
+    //  await client.close();
+}
+}
+
+
+
+
+
+app.get('/', (req, res) => {
+
+  res.send('Simple crud application is running red go!')
+
+})
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
+
+run().catch(console.dir)
